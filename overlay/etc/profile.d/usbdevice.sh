364e1584977de4f13ee0172c5dd4f6de171e6410
your name <your email>
2026-04-08 17:05:17 +0800

    modify

--- overlay/etc/profile.d/usbdevice.sh
+++ overlay/etc/profile.d/usbdevice.sh
@@ -3,7 +3,8 @@
 # The env variables below can be overridden
 
 # option: adb acm hid mtp ntb rndis uac1 uac2 ums uvc
-export USB_FUNCS="rndis"
+export USB_FUNCS="adb"
+#export USB_FUNCS="rndis"
 
 export UMS_FILE=/ums_shared.img
 export UMS_SIZE=256M
